0f405d87f1e657eb097b4daaee0da4d504cfb467
ouchi <misaki.ouchi17@gmail.com>
2023-09-08 16:02:18 +0900

    途中経過（しっかり/ざっくり途中）

--- js/dummy.js
+++ js/dummy.js
@@ -56,59 +56,6 @@ const adviseM = {
     }
   }
 }
-// const doubleOption = {
-//   template: `
-//     <div v-show="show" class="chat-select-opt2">
-//       <a v-show="area" :id="obj.a1" class="chat-option">
-//         <p :id="obj.a1" class="option-title">{{ obj.a1 }}</p>
-//         <p :id="obj.a1" class="option-detail">{{ obj.b1 }}<br>{{ obj.c1 }}</p>
-//       </a>
-//       <a v-show="area" :id="obj.a2" class="chat-option">
-//         <p :id="obj.a2" class="option-title">{{ obj.a2 }}</p>
-//         <p :id="obj.a2" class="option-detail">{{ obj.b2 }}<br>{{ obj.c2 }}</p>
-//       </a>
-//     </div>
-//   `,
-//   props: ['show', 'obj'],
-//   data: function () {
-//     return {
-//       area: false
-//     }
-//   },
-//   watch: {
-//     show: {
-//       handler(newData, oldData) {
-//         if (oldData !== newData) {
-//             this.area = true
-//         }
-//       }
-//     }
-//   }
-// }
-const selectOptions = {
-  template: `
-    <div v-show="show" v-for="item in list" class="chat-select-opt3">
-      <a v-show="area" :id="item" class="chat-option">
-          <p :id="item" class="option-detail">{{ item }}</p>
-      </a>
-    </div>
-  `,
-  props: ['show', 'list'],
-  data: function () {
-    return {
-      area: false
-    }
-  },
-  watch: {
-    show: {
-      handler(newData, oldData) {
-        if (oldData !== newData) {
-            this.area = true
-        }
-      }
-    }
-  }
-}
 const answerM = {
   template: `
   <div v-show="show" class="chat-area">
@@ -123,7 +70,7 @@ const answerM = {
                   </span>
               </p>
               <!-- 応答 -->
-              <p v-show="msg"><slot></slot></p>
+              <p v-show="msg">{{ message }}</p>
               <span class="chat-answer-after"></span>
               <!-- 既読 -->
               <span v-show="read" class="chat-answer-read">既読</span>
@@ -131,7 +78,7 @@ const answerM = {
       </div>
   </div>
   `,
-  props: ['show'],
+  props: ['show', 'message'],
   data: function () {
     return {
       area: false,
@@ -164,7 +111,6 @@ Vue.createApp({
     'advise-area': adviseArea, //アドバイザーエリア
     'adviser-img': adviserImg, //アドバイザー画像
     'advise-message': adviseM, //アドバイザーメッセージ
-    'select-options': selectOptions, //選択肢
     'answer-message': answerM, //選択後メッセージ
   },
   data: function () {
@@ -178,22 +124,21 @@ Vue.createApp({
         {
           name: 'calc',
           name2: '相場の計算',
-          msg0: ['２つの方法で相場を計算することができます。', 'どちらがご希望に近いですか？'], // type1のみ
-          msg: ['かしこまりました。', 'あなたの費用を、データをもとにざっくり計算します。'],
-          msgB: ['かしこまりました。', '一戸建てのお風呂リフォーム相場は', 'あなたの費用を、データをもとにしっかり計算します。'],
-          options0: { a1: 'ざっくり計算', b1: '広さや形状から', c1: 'おおまかに', a2: 'しっかり計算', b2: '欲しい機能や', c2: '設備も入れて' },
+          msg0: ['２つの方法で相場を計算することができます。', 'どちらがご希望に近いですか？'],
+          msg: [],
+          options: [],
           selected: '',
           type: 'type0',
-          branch: true,
-          show: [true]
+          show: []
         },
         {
           name: 'bathType',
           name2: 'お風呂の形式',
+          msg0: [],
           msg: ['希望されるお風呂は、どのような形式ですか？'],
           // options: [{ a: 'ユニットバス', b: 'タイル貼り', c: 'わからない' }],
           options: ['ユニットバス', 'タイル貼り', 'わからない'],
-          selected: [],
+          selected: '',
           type: 'type1',
           show: []
         },
@@ -203,7 +148,7 @@ Vue.createApp({
           msg0: [],
           msg: ['希望されるお風呂の大きさは、どのくらいですか？'],
           options: ['２畳未満', '２畳以上', 'わからない' ],
-          selected: [],
+          selected: '',
           type: 'type1',
           show: []
         },
@@ -213,31 +158,45 @@ Vue.createApp({
           msg0: ['浴槽まわりの希望をお伺いします。'],
           msg: ['湯船につかる頻度が多い場合は、浴槽の形が重要です。', '浴槽の形にこだわりはありますか？'],
           options: ['広さ重視', '節水重視', '特になし'],
-          selected: [],
+          selected: '',
           type: 'type3',
           show: []
         },
       ],
-      newList: [],
-      showList: [],
-      opt1Show: false
+      newList: [], // 表示用リスト
+      showList: [], // true/false格納リスト
+      firstOpt: [], // しっかり/ざっくりのtrue/false格納リスト
+      firstOptA: [] // しっかり計算表示用リスト
     };
   },
   created: function () {
     this.newList.push(this.list[0])
-    this.newList.push(this.list[1])
-    let push = () => this.showList.push(true)
-    this.interValFunc(push, 1, 1)
-    this.opt1Show = true
+    let push = () => this.list[0].show.push(true)
+    let push2 = () => this.firstOpt.push(true)
+    this.interValFunc(push, 2, 1)
+    this.timerFunc(push2, 4)
   },
   methods: {
-    selectFunc: function (e) {
-      // しっかり計算の場合
-      // if () {
-        
-      // }
-      // typeごとに表示方法変わる
-      // クリックしたら次の要素を配列に入れる
+    fstSelectFunc: function (e) {
+      // ざっくり/しっかり
+      let push2 = () => this.firstOpt.push(true)
+      let pushF = () => this.firstOpt.push(false)
+      if (e.target.dataset.opt === 'しっかり計算') {
+        this.list[0].selected = 'しっかり計算です'
+        this.firstOpt[0] = false
+        this.timerFunc(push2, 1)
+        this.interValFunc(push2, 6, 1)
+      }
+      if (e.target.dataset.opt === 'ざっくり計算') {
+        this.list[0].selected = 'ざっくり計算です'
+        this.firstOpt[0] = false
+        this.interValFunc(push2, 2, 1)
+        for (let i = 0; i < 7; i++){ pushF() }
+        console.log(this.list[0].show)
+        this.timerFunc(push2, 1)
+      }
+      this.newList.push(this.list[1])
+
     },
     // タイマー関数（実行関数、秒数）
     timerFunc: function (func, waitSeconds) {
@@ -249,7 +208,7 @@ Vue.createApp({
     },
     // 1秒ごとの繰り返し関数(実行関数、回数)
     interValFunc: function (func, count, seconds) {
-      let i = 0
+      let i = 1
       setInterval(() => {
         if (i <= count) {
           func()
